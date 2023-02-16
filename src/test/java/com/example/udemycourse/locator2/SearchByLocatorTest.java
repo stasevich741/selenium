04ed23c2f8eaea936ecd8e88862aa00b718e24bd
Stanislav Korolkov <stasevich741@gmail.com>
2023-02-16 17:51:13 +0300

    add locator

---
+++ src/test/java/com/example/udemycourse/locator2/SearchByLocatorTest.java
@@ -0,0 +1,70 @@
+package com.example.udemycourse.locator2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SearchByLocatorTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void close() {
+        driver.quit();
+    }
+
+    @Test
+    public void testByTagName() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+
+        WebElement textarea = driver.findElement(By.tagName("textarea"));
+        assertThat(textarea.getDomAttribute("rows")).isEqualTo("3");
+    }
+
+    @Test
+    public void testByHtmlAttributes() {
+        driver.get(
+                "https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+
+        // By name
+        WebElement textByName = driver.findElement(By.name("my-text"));
+        assertThat(textByName.isEnabled()).isTrue();
+
+        // By id
+        WebElement textById = driver.findElement(By.id("my-text-id"));
+        assertThat(textById.getAttribute("type")).isEqualTo("text");
+        assertThat(textById.getDomAttribute("type")).isEqualTo("text");
+        assertThat(textById.getDomProperty("type")).isEqualTo("text");
+
+        assertThat(textById.getAttribute("myprop")).isEqualTo("myvalue");
+        assertThat(textById.getDomAttribute("myprop")).isEqualTo("myvalue");
+        assertThat(textById.getDomProperty("myprop")).isNull();
+
+        // By public class name
+        List<WebElement> byClassName = driver
+                .findElements(By.className("form-control"));
+        assertThat(byClassName.size()).isPositive();
+        assertThat(byClassName.get(0).getAttribute("name"))
+                .isEqualTo("my-text");
+    }
+}
